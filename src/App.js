7a11d8c14e9b871830f5833984ff5c74c3307bae
Gayathri <gayathripaluru2002@gmail.com>
2025-09-15 12:50:49 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './routing/Navigation';
+import { ProductsProvider } from './contextAPI/ProductsContext'; 
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProductsProvider>
+      <div>
+        <Header/>
+        <Navigation/>
+        <Footer/>
+      </div>
+    </ProductsProvider>
   );
 }
-
-export default App;
+App;
